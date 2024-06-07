46670c2e2f0c2104be840eafc77834128e510643
Keigo Victor Fujita <keigofujita19@gmail.com>
2024-06-08 04:38:35 +0800

    Update BaseLayout.vue
    - Added Footer to the Layout

--- src/layouts/BaseLayout.vue
+++ src/layouts/BaseLayout.vue
@@ -1,12 +1,14 @@
+<script setup>
+import Navbar from "@/components/navigation/Navbar.vue";
+import Footer from "@/components/navigation/Footer.vue";
+</script>
+
 <template>
   <div class="font-inter">
     <Navbar />
     <main class="mt-16">
       <slot></slot>
     </main>
+    <Footer />
   </div>
 </template>
-
-<script setup>
-import Navbar from "@/components/navigation/Navbar.vue";
-</script>
