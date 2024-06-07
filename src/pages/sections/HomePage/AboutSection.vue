ecb6f8bdec77495930ea7893657eb98636df9a2b
Keigo Victor Fujita <keigofujita19@gmail.com>
2024-06-08 03:19:56 +0800

    Create AboutSection.vue
    - Created About Section

---
+++ src/pages/sections/HomePage/AboutSection.vue
@@ -0,0 +1,13 @@
+<script setup>
+import HeroImage from "@/assets/HeroImage.vue";
+import GithubIcon from "@/assets/icons/GithubIcon.vue";
+import EmailIcon from "@/assets/icons/EmailIcon.vue";
+import LinkedInIcon from "@/assets/icons/LinkedInIcon.vue";
+import Button from "@/components/ui/button/Button.vue";
+</script>
+
+<template>
+  <div class="mt-24">
+    <h1 class="text-6xl font-semibold mb-10">About Me</h1>
+  </div>
+</template>
