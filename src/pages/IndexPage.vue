e3f89abb95a071e2b7606151fac3498aeee1e8a8
Keigo Victor Fujita <keigofujita19@gmail.com>
2024-06-08 03:20:35 +0800

    Update IndexPage.vue
    - Removed the default layout of the sections
    - Added id's to each section for the scroll effect

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -3,15 +3,15 @@ import BaseLayout from "@/layouts/BaseLayout.vue";
 import HeroSection from "@/pages/sections/HomePage/HeroSection.vue";
 import SkillsSection from "@/pages/sections/HomePage/SkillsSection.vue";
 import ProjectsSection from "@/pages/sections/HomePage/ProjectsSection.vue";
+import AboutSection from "@/pages/sections/HomePage/AboutSection.vue";
 </script>
 <template>
   <BaseLayout>
     <div class="max-w-6xl mx-auto pb-24">
-      <HeroSection />
-      <div class="space-y-24">
-        <SkillsSection />
-        <ProjectsSection />
-      </div>
+      <HeroSection id="hero-section" />
+      <SkillsSection id="skills-section" />
+      <ProjectsSection id="projects-section" />
+      <AboutSection id="about-section" />
     </div>
   </BaseLayout>
 </template>
