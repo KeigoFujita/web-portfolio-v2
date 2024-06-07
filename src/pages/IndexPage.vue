52d2e39c848a6ee5bdeea6319a707794f6367027
Keigo Victor Fujita <keigofujita19@gmail.com>
2024-06-08 04:38:23 +0800

    Update IndexPage.vue
    - Removed the padding bottom on Index Page
    - Added Contact Section aside from the max-w-xl section

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -4,14 +4,16 @@ import HeroSection from "@/pages/sections/HomePage/HeroSection.vue";
 import SkillsSection from "@/pages/sections/HomePage/SkillsSection.vue";
 import ProjectsSection from "@/pages/sections/HomePage/ProjectsSection.vue";
 import AboutSection from "@/pages/sections/HomePage/AboutSection.vue";
+import ContactSection from "@/pages/sections/HomePage/ContactSection.vue";
 </script>
 <template>
   <BaseLayout>
-    <div class="max-w-6xl mx-auto pb-24">
+    <div class="max-w-6xl mx-auto">
       <HeroSection id="hero-section" />
       <SkillsSection id="skills-section" />
       <ProjectsSection id="projects-section" />
       <AboutSection id="about-section" />
     </div>
+    <ContactSection id="contact-section" />
   </BaseLayout>
 </template>
