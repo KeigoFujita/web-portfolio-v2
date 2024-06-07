1498db0cf3449711f680bd685af48061860df62d
Keigo Victor Fujita <keigofujita19@gmail.com>
2024-06-08 04:37:25 +0800

    Update Footer.vue
    Created Footer Design

--- src/components/navigation/Footer.vue
+++ src/components/navigation/Footer.vue
@@ -1,71 +1,92 @@
+<script setup>
+import logo from "@/assets/logo.png";
+import Button from "@/components/ui/button/Button.vue";
+import GmailIcon from "@/assets/icons/GmailIcon.vue";
+import FacebookIcon from "@/assets/icons/FacebookIcon.vue";
+import InstagramIcon from "@/assets/icons/InstagramIcon.vue";
+import LinkedInIcon from "@/assets/icons/LinkedInIcon.vue";
+import GithubIcon from "@/assets/icons/GithubIcon.vue";
+</script>
 <template>
-  <footer
-    class="rounded-lg shadow bg-gradient-to-br from-purple-400 to-blue-300 m-4 transition-all duration-1000 ease-in-out"
-  >
-    <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-      <div class="sm:flex sm:items-center sm:justify-between">
-        <img
-          :src="cnxlogo"
-          class="w-96 mix-blend-multiply"
-        />
-        <!-- <a
-          href="/"
-          class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
-        >
+  <footer class="bg-[#0F0F11] py-8">
+    <div class="max-w-6xl mx-auto">
+      <div class="flex justify-between">
+        <div class="flex items-center space-x-4">
           <img
-            src="https://flowbite.com/docs/images/logo.svg"
-            class="h-8"
+            class="h-12 w-12"
+            :src="logo"
+            alt="Keigo Victor FUjita"
           />
-          <span class="self-center text-2xl font-semibold whitespace-nowrap"
-            >Concentrix + Webhelp</span
+          <h1 class="text-xl dark:text-white font-semibold">KEIGO FUJITA</h1>
+        </div>
+        <div>
+          <div class="text-xs text-neutral-600 text-center">
+            <p>Version 2.0</p>
+            <p>Copyright 2024 © Keigo Fujita</p>
+            <p>All Rights Reserved</p>
+          </div>
+        </div>
+        <div class="flex space-x-2">
+          <Button
+            as="a"
+            href="mailto:keigofujita19@gmail.com"
+            target="_blank"
+            variant="secondary"
+            class="bg-[#171717] rounded-full px-0 py-0 h-11"
           >
-        </a> -->
-        <ul
-          class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0"
-        >
-          <li>
-            <a
-              href="#"
-              class="hover:underline me-4 md:me-6"
-              >About</a
-            >
-          </li>
-          <li>
-            <a
-              href="#"
-              class="hover:underline me-4 md:me-6"
-              >Privacy Policy</a
-            >
-          </li>
-          <li>
-            <a
-              href="#"
-              class="hover:underline me-4 md:me-6"
-              >Licensing</a
-            >
-          </li>
-          <li>
-            <a
-              href="#"
-              class="hover:underline"
-              >Contact</a
-            >
-          </li>
-        </ul>
+            <div class="px-3 flex items-center space-x-2 rounded-lg text-white">
+              <GmailIcon class="w-5 h-5" />
+            </div>
+          </Button>
+
+          <Button
+            as="a"
+            href="https://web.facebook.com/keigo.fujita.94"
+            target="_blank"
+            variant="secondary"
+            class="bg-[#171717] rounded-full px-0 py-0 h-11"
+          >
+            <div class="px-3 flex items-center space-x-2 rounded-lg text-white">
+              <FacebookIcon class="w-5 h-5" />
+            </div>
+          </Button>
+          <Button
+            as="a"
+            href="https://www.instagram.com/_keigofujita/"
+            target="_blank"
+            variant="secondary"
+            class="bg-[#171717] rounded-full px-0 py-0 h-11"
+          >
+            <div class="px-3 flex items-center space-x-2 rounded-lg text-white">
+              <InstagramIcon class="w-5 h-5" />
+            </div>
+          </Button>
+
+          <Button
+            as="a"
+            href="https://www.linkedin.com/in/keigo-fujita-2a1058143/"
+            target="_blank"
+            variant="secondary"
+            class="bg-[#171717] rounded-full px-0 py-0 h-11"
+          >
+            <div class="px-3 flex items-center space-x-2 rounded-lg text-white">
+              <LinkedInIcon class="w-5 h-5" />
+            </div>
+          </Button>
+
+          <Button
+            as="a"
+            href="https://github.com/KeigoFujita"
+            target="_blank"
+            variant="secondary"
+            class="bg-[#171717] rounded-full px-0 py-0 h-11"
+          >
+            <div class="px-3 flex items-center space-x-2 rounded-lg text-white">
+              <GithubIcon class="w-5 h-5" />
+            </div>
+          </Button>
+        </div>
       </div>
-      <hr class="my-6 border-gray-700 sm:mx-auto lg:my-8" />
-      <span class="block text-sm text-gray-500 sm:text-center"
-        >© 2024
-        <a
-          href="/"
-          class="hover:underline"
-          >Business Analytics™</a
-        >. All Rights Reserved.</span
-      >
     </div>
   </footer>
 </template>
-
-<script setup>
-import cnxlogo from "@/assets/cnxlogo.png";
-</script>
